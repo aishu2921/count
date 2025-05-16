ea8cd0e5a6e14454019a2bb5324ed46d78136c4a
aishu2921 <aishusowmi2005@gmail.com>
2025-05-16 12:12:52 +0530

    first commit

---
+++ exlink.js
@@ -0,0 +1,6 @@
+console.log(window);
+console.log(document);
+setTimeout(() => {
+                      document.getElementById("counts").innerHTML = "Happy Birthday! Wishing you a day filled with joy, laughter, and everything wonderful. May this year bring you happiness, success, and all your heart's desires";
+                      console.log("Happy Birthday! Wishing you a day filled with joy, laughter, and everything wonderful. May this year bring you happiness, success, and all your heart's desires");
+                    }, 12000);
